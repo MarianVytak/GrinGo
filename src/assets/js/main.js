1c9a232e3b2615508c9bd930d6f45f02fc61433c
MarianVytak <marianvytak@gmail.com>
2020-05-24 16:43:09 +0300

    Added Section 1.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,2 +1,3 @@
-//= components/hamburger.js
 //= components/customs.js
+//= components/hamburger.js
+//= ../../templates/section-1/section-1.js
---
+++ src/templates/section-1/section-1.js
@@ -0,0 +1,8 @@
+$('.section-1__slider').slick({
+    centerMode: true,
+    centerPadding: '185px',
+    slidesToShow: 1,
+    arrows: false,
+    dots: true,
+    focusOnSelect: true
+});
