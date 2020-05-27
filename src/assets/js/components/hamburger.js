ab5bb3168628e92f9a8493f4d93405b9249b8823
MarianVytak <marianvytak@gmail.com>
2020-05-27 13:46:58 +0300

    Added open Side Menu and Slider arrows.

--- src/assets/js/components/hamburger.js
+++ src/assets/js/components/hamburger.js
@@ -1,5 +1,7 @@
 const hamburger = $('.hamburger');
+const sideMenu = $('.side-menu');
 
 hamburger.on('click', function(){
     hamburger.toggleClass('hamburger-active');
+    sideMenu.toggleClass('active');
 });
