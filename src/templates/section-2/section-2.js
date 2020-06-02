cdffabf5756b21629fccc92b6952351ca6d1f658
MarianVytak <marianvytak@gmail.com>
2020-06-02 04:16:11 +0300

    New Version

--- src/templates/section-2/section-2.js
+++ src/templates/section-2/section-2.js
@@ -6,26 +6,26 @@ $('.section-2__slider').slick({
     arrows: false,
     dots: false,
     focusOnSelect: true,
-    responsive: [
-            {
-                breakpoint: 1300,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    arrows: false,
-                    centerPadding: '5vw',
-                }
-            },
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 1,
-                    variableWidth: true,
-                    slidesToScroll: 1,
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '0'
-                }
-            }
-        ]
+    // responsive: [
+    //         {
+    //             breakpoint: 1300,
+    //             settings: {
+    //                 slidesToShow: 3,
+    //                 slidesToScroll: 1,
+    //                 arrows: false,
+    //                 centerPadding: '5vw',
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 992,
+    //             settings: {
+    //                 slidesToShow: 1,
+    //                 variableWidth: true,
+    //                 slidesToScroll: 1,
+    //                 arrows: false,
+    //                 centerMode: true,
+    //                 centerPadding: '0'
+    //             }
+    //         }
+    //     ]
 });
