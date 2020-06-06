6f82b17d566c370487aa23dfe83ce084b5c5f0cf
MarianVytak <marianvytak@gmail.com>
2020-06-06 17:40:55 +0300

    Section 3: закріпив при скролі.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,4 +2,5 @@
 //= components/hamburger.js
 //= ../../templates/section-1/section-1.js
 //= ../../templates/section-2/section-2.js
+//= ../../templates/section-3/section-3.js
 //= ../../templates/fast-deposit/fast-deposit.js
---
+++ src/templates/section-3/section-3.js
@@ -0,0 +1,22 @@
+// Scroll Section 3
+const scrollSection3 = $('.section-3');
+const scrollSection3OffsetTop = scrollSection3.offset().top;
+const headerHeight = $('.header').height();
+
+$(document).on('scroll', function () {
+    let thisScroll = $(this).scrollTop();
+    if (thisScroll > scrollSection3OffsetTop - headerHeight) {
+        scrollSection3.addClass('active');
+    } else {
+        scrollSection3.removeClass('active');
+    }
+});
+// $(window).scroll(function(){
+//     let thisScroll = $(this).scrollTop();
+//     if (thisScroll >= headerHeight) {
+//         scrollSection3.removeClass('active');
+//     }
+//     else {
+//         scrollSection3.addClass('active');
+//     }
+// });
